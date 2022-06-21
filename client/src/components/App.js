c6ed0160d714850e933c621960f12bfabcc4f036
Hyun Sung Cho <hcho774@gmail.com>
2022-06-21 11:47:00 -0400

    SASS installed for use with all components ready to be worked on in client side

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,10 @@
-import "./App.css";
+import Dashboard from "./Dashboard";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>hello</h1>
+        <Dashboard />
       </header>
     </div>
   );
---
+++ client/src/components/ProductList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProductList() {
+  return <div>Product</div>;
+}
+
+export default Product;
